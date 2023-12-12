2f511cc898b68507e222fef54b5f153c0d4b4b27
Baptiste Duvieu <baptiste.duvieu1@etu.univ-lehavre.fr>
2023-12-12 19:00:24 +0100

    debut commentaires

--- tree.scala
+++ tree.scala
@@ -12,16 +12,30 @@ case class SimpleRNG(seed: Long) extends RNG {
 }
 
 sealed trait Tree[+A] {
+
+    /**
+    * Méthode pour connaître la taille de l'arbre
+    * +1 à chaque branche et chaque feuille
+    */
     def size: Int = this match {
         case Leaf(_) => 1
         case Branch(l,r) => 1 + l.size + r.size 
     }
 
+    /**	
+    * Méthode pour connaître le nombre de feuilles de l'arbre
+    * +1 à chaque feuille
+    */
     def count: Int = this match {
         case Leaf(_) => 1
         case Branch(l,r) => l.count + r.count 
     }
 
+    /**	
+    * Méthode pour connaître le maximum de l'arbre
+    * parcours récursif de l'arbre pour avoir les valeurs de chaque feuille
+    * comparaison avec f
+    */
     def max(f: (A, A) => Boolean): A = this match {
         case Leaf(v) => v
         case Branch(l, r) => if(f(l max f, r max f)) r max f else l max f
@@ -97,22 +111,38 @@ object TestTree {
 
     def main(args: Array[String]): Unit = {
 
+        /* Impossible de faire un arbre vide car pas de gestion de none (option)
+        * Leaf hérite de Tree donc c'est possible de faire un arbre avec une seule feuille (b)
+        * Idem pour les arbres à nbre de feuilles quelconques, il suffira de bien manipuler Leaf et Branch selon la topologie voulue
+        */
+        /*val void = Branch()*/
+        val single = Leaf(42)
+        val random = Branch(Branch(Leaf(2), Leaf(8)), Branch(Leaf(13), Leaf(4)))
+
+        println(single)
+        println(random)
+
         val a = Branch(Branch(Branch(Leaf(4), Leaf(5)), Leaf(8)), Branch(Leaf(13), Leaf(9)))
         val b = Branch(Branch(Branch(Leaf("c"), Leaf("p")), Leaf("x")), Branch(Leaf("q"), Leaf("a")))
 
+        println()
         println(a)
         println("Taille de a = " + a.size)
         println("Nombre de feuilles de a = " + a.count)
         println("Max de a = " + max(a))
         println("Max de a avec méthode = " + a.max((a: Int,b: Int) => a < b))
+        println()
+        println(b)
         println("Max de b = " + b.max((a: String,b: String) => a < b))
-
+        println()
+        
         println("Cherchons 8 dans a :")
         val sa = a.find(v => v == 8)
         sa match {
            case Some(v) => println(s"La valeur $v est bien dans a !")
            case None => println("Cette valeur n'est pas dans a !")
         }
+        println()
 
         println("Cherchons o dans b :")
         val sb = b.find(v => v == "o")
@@ -120,13 +150,15 @@ object TestTree {
            case Some(v) => println(s"La valeur $v est bien dans b !")
            case None => println("Cette valeur n'est pas dans b !")
         }
+        println()
 
         println("Profondeur de a = " + a.depth)
-
+        println()
         val c = a.map(_+1)
         println("Map de a : " + c)
         val d = b.map(_+"a")
         println("Map de b : " + d)
+        println()
         println("Taille de a avec size2 : " + a.size2)
         println("Nombre de feuilles de a avec count2 : " + a.count2)
         println("Max de a avec max2 : " + a.max2((a: Int,b: Int) => a < b))
@@ -136,8 +168,9 @@ object TestTree {
         println("Map2 de a : " + e)
         val f = b.map2(_+"a")
         println("Map2 de b : " + f)
+        println()
         println("a sous forme de liste : " + a.toList)
-
+        println()
         val rng = SimpleRNG(54)
         println("Valeur d'une feuille aléatoire de a : " + a.rand(rng))
         val rng2 = SimpleRNG(108)
